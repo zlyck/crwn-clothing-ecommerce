a4199d15c30060fafdb5f3728a4bcf5bbb15f2a8
Zlyck <zlyckdevelopments@gmail.com>
2021-03-29 12:02:25 +0800

    Added Shop page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
 import {Switch,Route} from 'react-router-dom';
+import ShopPage from './pages/shop/shop.component';
 import HomePage from './pages/homepage/homepage.components';
 
 
@@ -9,6 +10,7 @@ function App() {
     <div>
       <Switch>
         <Route exact path='/' component={HomePage}/>
+        <Route  path='/shop' component={ShopPage}/>
       </Switch>
     </div>
   );
