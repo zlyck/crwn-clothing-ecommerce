d9b28a38a35a84793b8190cc9770eeebc48d6e74
Ernest Siega <ernestsiega@Ernests-iMac.local>
2021-04-06 14:26:16 +0800

    Finishing sign in method

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import CustomButton from '../custom-button/custom-button.component';
 import FormInput from '../form-input/form-input.component';
-import {signInWithGoogle} from '../../firebase/firebase.utils';
+import {auth,signInWithGoogle} from '../../firebase/firebase.utils';
 import './sign-in.styles.scss';
 
 
@@ -17,11 +17,21 @@ class SignIn extends React.Component{
         
     }
 
-    handleSubmit = event =>{
+    handleSubmit = async event =>{
         event.preventDefault();
+        const {email,password}=this.state;
+        
+        try{
 
-        this.setState({email: '', password: ''})    
-    }
+            await auth.signInWithEmailAndPassword(email,password);
+            this.setState({email: '', password: ''})
+
+        }catch(error){
+            console.log(error);
+        }
+        
+            
+    };
     
     handleChange = event =>{
             const {value, name} = event.target;
