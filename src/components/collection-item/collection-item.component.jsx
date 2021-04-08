59f260d5b8908bad9a81d7c65e34919d510b0313
Zlyck <zlyckdevelopments@gmail.com>
2021-04-08 13:19:18 +0800

    Added quantity to products

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
-
+import CustomButton from '../custom-button/custom-button.component';
+import {connect} from 'react-redux';
+import {addItem} from '../../redux/cart/cart.actions';
 import './collection-item.styles.scss';
+import { findRenderedComponentWithType } from 'react-dom/test-utils';
 
-const CollectionItem = ({id, name, price, imageUrl}) => (
+const CollectionItem = ({item, addItem}) => {
+    const {name, price, imageUrl} = item;
+    return(
     <div className='collection-item'>
         <div
         className='image'
@@ -14,8 +19,16 @@ const CollectionItem = ({id, name, price, imageUrl}) => (
             <span className='name'>{name}</span>
             <span className='price'>{price}</span>
         </div>
+        <CustomButton onClick={()=>addItem(item)} inverted>
+        Add to cart
+        </CustomButton>
         
     </div>
-);
 
-export default CollectionItem;
+       );
+}
+const mapDispatchToProps = dispatch => ({
+    addItem: item => dispatch(addItem(item))
+});
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
