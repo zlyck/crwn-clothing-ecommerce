22fb13874abbe4c7985ee2ee2516b076ce1245a9
Ernest Siega <ernestsiega@Ernests-iMac.local>
2021-04-15 02:22:47 +0800

    moved directory to its own reducer

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import CollectionItems from '../../components/collection-item/collection-item.component';
+import {selectCollections} from '../../redux/shop/shop.selectors';
+import './collection.styles.scss';
+
+
+const CollectionPage = ({collection}) => {
+    const {title, items} = collection;
+   return (<div className='collection'>
+        <h2 className='title'>{title}</h2>
+        <div className='items'>
+            {
+                items.map(
+                    (item => <CollectionItems key={item.id} item={item}/>
+                        ))
+            }
+        </div>
+    </div>)
+};
+
+const mapStateToProps = (state, ownProps) => ({
+    collection: selectCollections(ownProps.match.params.collectionId)(state)
+});
+export default connect(mapStateToProps)(CollectionPage);
